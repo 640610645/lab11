5b696c217979ebe6c7116e51a3cbb7e9382fcd4a
nchanadee <nontapan_chanadee@cmu.ac.th>
2021-12-17 19:17:52 +0700

    lab11.3

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,38 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <cstdlib>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+    int count = 0;
+    double sum = 0;
+    ifstream source("score.txt");
+    string t;
+    cout << "Number of data = ";
+    while(getline(source,t)){
+        sum += atof(t.c_str());
+        count++;
+    }
+    cout << count << endl;
+    cout << setprecision(3);
+    double mean = sum/count;
+    cout << "Mean = "<< mean << endl;
+    cout << "Standard deviation = ";
+    double sd,y;
+    getline(source,t);
+    double x = atof(t.c_str());
+    for(int i = 0; i < count; i++){
+        y += pow(x,2);
+    }
+    sd = (y/count) - pow(mean,2);
+    cout << sd;
+
+
+
+    
+    
+
+}
